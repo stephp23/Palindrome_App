d582fb93dbd370523e76093aaaaff2f1b29a8b30
Stephanie Pena <stephanie.pena@stephanies-mbp.myfiosgateway.com>
2020-10-12 23:40:10 -0400

    Palindrome App JS file

---
+++ PalindromeApp.js
@@ -0,0 +1,16 @@
+
+function isItPalindrome() {
+ let initialWordInput = prompt("What word would you liek to check to see if it's a palindrome?");
+  let initialWord = initialWordInput.toLowerCase();
+  let initialWordReverse = initialWord.split('').reverse().join('');
+  
+  if (initialWord == initialWordReverse) {
+    alert(`The word ${initialWordInput} is a palindrone!`)
+  } else {
+    alert(`The word ${initialWordInput} is not palindrone.`)
+  }
+  
+}
+
+isItPalindrome();
+
