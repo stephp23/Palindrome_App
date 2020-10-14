6398fcd4dee9ef2bd8023703651a1cf9a54b175b
Stephanie Pena <stephanie.pena@Stephanies-MacBook-Pro.local>
2020-10-14 14:19:12 -0400

    App works! Fixed button and wordCollector. App now stores inout into value variable. Made changes to css styling.

--- PalindromeApp.js
+++ PalindromeApp.js
@@ -6,28 +6,28 @@ submitButton.addEventListener('click', isItPalindrome);
 
 function isItPalindrome(inputData) {
   //let initialWordInput = prompt("What word would you like to check to see if it's a palindrome?");
-   let initialWord = inputData.toString().toLowerCase();
-   let initialWordReversed = inputData.toString().split('').reverse().join('');
+  function isPalindrome(inputData) {
+    let initialWord = inputData.toLowerCase();
+    let initialWordReversed = initialWord.toString().split('').reverse().join('');
+
+    //compares initial word to the same word in reverse, definition of a palindrome
+    return initialWord == initialWordReversed ? true : false;
+    
+  };
    
-  //  if (initialWord == initialWordReversed) {
-  //    alert(`The word ${inputData} is a palindrone!`)
-  //  } else {
-  //    alert(`The word ${inputData} is not palindrone.`)
-  //  }
-   
+  //collects the input value given by user, and stores it in variable value
+  let value = document.getElementById('wordCollector').value;
+  //replaces text in the answer display region with the text stored in variale notification
+  let notification = document.getElementById('displayPanel');
+
+  if (isPalindrome(value) === true) {
+    console.log(`The word ${value} is a palindrone!`)
+    notification.innerHTML = `The word ${value} is a palindrone!`;
+  } else {
+    console.log(`The word ${value} is not a palindrone.`)
+    notification.innerHTML = `The word ${value} is not a palindrone!`;
+  }
+
+  inputData.preventDefault(inputData);
+  
 };
- 
-//collects the input value given by user, and stores it in variable value
-let value = document.getElementById('wordCollector').value;
-//replaces text in the answer display region with the text stored in variale notification
-let notification = document.getElementById('displayPanel');
-
-if (initialWord == initialWordReversed) {
-  console.log(`The word ${inputData} is a palindrone!`)
-  notification.innerHTML = `The word ${inputData} is a palindrone!`;
-} else {
-  console.log(`The word ${inputData} is not a palindrone.`)
-  notification.innerHTML = `The word ${inputData} is not a palindrone!`;
-}
-
-inputData.preventDefault(inputData);
