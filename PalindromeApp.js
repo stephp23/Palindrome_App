1911011e287b4be093b51bcfeac20f74009ef763
Stephanie Pena <stephanie.pena@Stephanies-MacBook-Pro.local>
2020-10-14 13:06:07 -0400

    updates on html and css styling, but result from palindrome app still does not display

--- PalindromeApp.js
+++ PalindromeApp.js
@@ -1,80 +1,33 @@
-
+//submission button
 let submitButton = document.getElementById('submitButton');
+//calls Palindrome fuction to act when the button is pressed
 submitButton.addEventListener('click', isItPalindrome);
-
 
 
 function isItPalindrome(inputData) {
   //let initialWordInput = prompt("What word would you like to check to see if it's a palindrome?");
-   let initialWord = initialWordInput.toLowerCase();
-   let initialWordReverse = initialWord.split('').reverse().join('');
+   let initialWord = inputData.toString().toLowerCase();
+   let initialWordReversed = inputData.toString().split('').reverse().join('');
    
-   if (initialWord == initialWordReverse) {
-     alert(`The word ${initialWordInput} is a palindrone!`)
-   } else {
-     alert(`The word ${initialWordInput} is not palindrone.`)
-   }
+  //  if (initialWord == initialWordReversed) {
+  //    alert(`The word ${inputData} is a palindrone!`)
+  //  } else {
+  //    alert(`The word ${inputData} is not palindrone.`)
+  //  }
    
- }
+};
  
-isItPalindrome();
-
+//collects the input value given by user, and stores it in variable value
 let value = document.getElementById('wordCollector').value;
+//replaces text in the answer display region with the text stored in variale notification
 let notification = document.getElementById('displayPanel');
 
+if (initialWord == initialWordReversed) {
+  console.log(`The word ${inputData} is a palindrone!`)
+  notification.innerHTML = `The word ${inputData} is a palindrone!`;
+} else {
+  console.log(`The word ${inputData} is not a palindrone.`)
+  notification.innerHTML = `The word ${inputData} is not a palindrone!`;
+}
 
-
- 
-// // Create a "close" button and append it to each list item
-// let myNodelist = document.getElementsByTagName("LI");
-// let i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   let span = document.createElement("SPAN");
-//   let txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-// // Click on a close button to hide the current list item
-// let close = document.getElementsByClassName("close");
-// let i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//   let div = this.parentElement;
-//   div.style.display = "none";
-//   }
-// }
-
-
-
-
-
-// // Create a new list item when clicking on the "Add" button
-// function newElement() {
-//   let li = document.createElement("li");
-//   let inputValue = document.getElementById("myInput").value;
-//   let t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   if (inputValue === '') {
-//    alert("You must write something!");
-//   } else {
-//     document.getElementById("myUL").appendChild(li);
-//   }
-//   document.getElementById("myInput").value = "";
-//   let span = document.createElement("SPAN");
-//   let txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//         let div = this.parentElement;
-//         div.style.display = "none";
-//     }
-//   }
-// }
-// //Clearing the list
-// function removeAll(){
-//   var lst = document.getElementsByTagName("ul");
-//     lst[0].innerHTML = "";
-// }
+inputData.preventDefault(inputData);
